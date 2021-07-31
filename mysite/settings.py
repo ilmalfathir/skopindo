042a8545bd4bd84362921a134e6dabaee2ff58d2
Ilmal Sabda Fathir <88005162+ilmalfathir@users.noreply.github.com>
2021-08-01 06:05:51 +0700

    allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6z4=aao2=!ue@dev7zx5knof=(7!zn3c8zj%10#dmnhem$4w6*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
