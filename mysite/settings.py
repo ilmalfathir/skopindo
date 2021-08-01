60cda2e341fff823a2cfd804ca6efd34263040cf
Ilmal Sabda Fathir <88005162+ilmalfathir@users.noreply.github.com>
2021-08-01 07:19:14 +0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
